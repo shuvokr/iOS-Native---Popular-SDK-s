2b276b694fc3f7ab5e63f41b22df6ae637e39d2f
Hex_aad <shuvosksk@gmail.com>
2020-11-08 13:26:48 +0600

    Added login & signup page.

---
+++ MartVenue/AuthLoginSignup/LoginSignUpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginSignUpViewController.swift
+//  MartVenue
+//
+//  Created by Shuvo on 8/11/20.
+//
+
+import UIKit
+
+class LoginSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

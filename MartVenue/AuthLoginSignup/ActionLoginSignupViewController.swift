cfbadbb6da64a6907d6c7abdb7efe2562e43b0b8
Hex_aad <shuvosksk@gmail.com>
2020-11-08 16:21:26 +0600

    Config tab bar for HOME, CATEGORY, OFFERS & ACCOUNT

--- MartVenue/AuthLoginSignup/ActionLoginSignupViewController.swift
+++ MartVenue/AuthLoginSignup/ActionLoginSignupViewController.swift
@@ -9,19 +9,33 @@ import UIKit
 
 class ActionLoginSignupViewController: UIViewController {
     
+    @IBOutlet weak var loginButton: UIButton!
+    
     var navTitle: String = "No Title"
+    let tabHomeNavId = "segue_id_tabbar_home"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.navigationItem.title = navTitle
+        if(navTitle == "Login") {
+            loginButton.isHidden = false
+        }
+        else {
+            loginButton.isHidden = true
+        }
     }
     
     @IBAction func goBack(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func loginSignupAction(_ sender: Any) {
+        if(navTitle == "Login") {
+            self.performSegue(withIdentifier: tabHomeNavId, sender: self)
+        }
+    }
     /*
     // MARK: - Navigation
 
